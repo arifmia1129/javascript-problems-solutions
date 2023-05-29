eccf7ba5c21cd03107c4caacc20093a7cdec01c7
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-29 18:05:58 +0600

    function for reverse string

--- index.js
+++ index.js
@@ -22,17 +22,28 @@
 // console.log(largestNumber([1, 2, 3,10,100,5]));
 
 // technique:-2
-function largestNumber(arr) {
-    let largeNumber = arr[0];
-    for(const num of arr) {
-        if(num > largeNumber) {
-            largeNumber = num;
-        }
-    }
-    return largeNumber;
-}
+// function largestNumber(arr) {
+//     let largeNumber = arr[0];
+//     for(const num of arr) {
+//         if(num > largeNumber) {
+//             largeNumber = num;
+//         }
+//     }
+//     return largeNumber;
+// }
 
-console.log(largestNumber([1, 2, 3,10,100,500]));
+// console.log(largestNumber([1, 2, 3,10,100,500]));
+
+
+// Problem 3:
+// Write a function that takes a string as input and returns the reverse of the string.
+
+// const reverseString = (str)=> {
+//     return str.split("").reverse().join("");
+// }
+
+// console.log(reverseString("Arif"));
+
 
 
 

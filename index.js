51a89bf4dacfc04d43224d8408253d3794000f62
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-29 18:02:22 +0600

    function for get largest number from array of numbers

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 // Problem 1:
 // Write a function that takes two numbers as input and returns their sum.
 
-function sumNumbers(num1, num2) {
-    return num1 + num2;
+// function sumNumbers(num1, num2) {
+//     return num1 + num2;
+// }
+
+// const sum = sumNumbers(10, 20);
+
+// console.log(sum);
+
+
+// Problem 2:
+// Write a function that takes an array of numbers as input and returns the largest number in the array.
+
+// technique:-1
+
+// function largestNumber (arr) {
+//     return Math.max(...arr);
+// }
+
+// console.log(largestNumber([1, 2, 3,10,100,5]));
+
+// technique:-2
+function largestNumber(arr) {
+    let largeNumber = arr[0];
+    for(const num of arr) {
+        if(num > largeNumber) {
+            largeNumber = num;
+        }
+    }
+    return largeNumber;
 }
 
-const sum = sumNumbers(10, 20);
+console.log(largestNumber([1, 2, 3,10,100,500]));
 
-console.log(sum);
+
+
+
+

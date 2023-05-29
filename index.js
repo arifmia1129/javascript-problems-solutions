336eecf094710c00f37654527e1241e146d1d6b8
Md. Arif Mia <arif.vtti@gmail.com>
2023-05-29 17:57:57 +0600

    solved problem to write function for sum of two numbers

---
+++ index.js
@@ -0,0 +1,10 @@
+// Problem 1:
+// Write a function that takes two numbers as input and returns their sum.
+
+function sumNumbers(num1, num2) {
+    return num1 + num2;
+}
+
+const sum = sumNumbers(10, 20);
+
+console.log(sum);
